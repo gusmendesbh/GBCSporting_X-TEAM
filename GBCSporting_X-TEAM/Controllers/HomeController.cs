9fca2310e0de876516e37593e8c1d007353d24a3
Gustavo Mendes Pinto <101278278@georgebrown.ca>
2022-02-01 18:32:44 -0500

    Working - Feb2 6:32 (Gus)

---
+++ GBCSporting_X-TEAM/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using GBCSporting_X_TEAM.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace GBCSporting_X_TEAM.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Products()
+        {
+            return View();
+        }
+
+        public IActionResult Technicians()
+        {
+            return View();
+        }
+
+        public IActionResult Customers()
+        {
+            return View();
+        }
+
+        public IActionResult Incidents()
+        {
+            return View();
+        }
+
+        public IActionResult Registrations()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
