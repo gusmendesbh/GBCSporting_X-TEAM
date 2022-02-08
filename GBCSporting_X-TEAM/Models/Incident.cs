69d415f7b3d9d6eb34a0d8cfc374352082159e28
ROB <kaczurr@gmail.com>
2022-02-08 14:51:40 -0500

    Added models and DbContext

---
+++ GBCSporting_X-TEAM/Models/Incident.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GBCSporting_X_TEAM.Models
+{
+    public class Incident
+    {
+        [Key]
+        public int IncidentId { get; set; }
+        [Required]
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+        [Required]
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public DateTime DateOpened { get; set; }
+        public DateTime DateClosed { get; set; }
+        public int? TechnicianId { get; set; }
+        public Technician Technician { get; set; }
+    }
+}
---
+++ GBCSporting_X-TEAM/Models/Technician.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GBCSporting_X_TEAM.Models
+{
+    public class Technician
+    {
+        [Key]
+        public int TechnicianId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Phone { get; set; }
+
+    }
+}
