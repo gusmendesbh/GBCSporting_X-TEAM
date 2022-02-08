1c77a70b60259e089e3129af4a3e625a7c4005fa
ROB <kaczurr@gmail.com>
2022-02-08 15:04:32 -0500

    Added dependency injection for dbcontext and corrected nuget package

--- GBCSporting_X-TEAM/Program.cs
+++ GBCSporting_X-TEAM/Program.cs
@@ -1,9 +1,22 @@
+using GBCSporting_X_TEAM.Models;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddRouting(options =>
+{
+    options.LowercaseUrls = true;
+    options.AppendTrailingSlash = true;
+});
+
+builder.Services.AddDbContext<GbcSportingContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("GbcSportingContext"))
+    );
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
